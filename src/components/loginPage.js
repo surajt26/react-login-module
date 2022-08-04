34b8b147d6835c88bea2f41b5d6be36c51143858
Suraj KY <face.surajyadav@gmail.com>
2022-08-04 13:22:48 +0530

    make basic loginForm and add getInputHandle method in loginPage Component

--- src/components/loginPage.js
+++ src/components/loginPage.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const loginPage = () => {
-  return (
-    <div>loginPage</div>
-  )
+const LoginPage = () => {
+
+    // useState for store loginForm inputs
+    const [loginForm, setLoginForm] = useState({});
+
+    // getInputHandle for handle input onChange
+    const getInputHandle = (event) => {
+        // object destructuring
+        const { name, value } = event.target;
+        setLoginForm((preValue) => ({
+            ...preValue,
+            [name]: value
+        }))
+    }
+
+    return (<>
+        <form name='loginForm'>
+            <label htmlFor='EmailId'>Email ID</label><br />
+            <input
+                type='email'
+                id='EmailId'
+                name='email'
+                value={loginForm.email || ''}
+                onChange={getInputHandle}
+                required
+            /><br />
+            <label htmlFor='Password'>Password</label><br />
+            <input
+                type='password'
+                id='Password'
+                name='password'
+                value={loginForm.password || ''}
+                onChange={getInputHandle}
+                required
+            /><br />
+            <input
+                type='submit'
+                value='Login'
+            />
+        </form>
+    </>)
 }
 
-export default loginPage
+export default LoginPage
