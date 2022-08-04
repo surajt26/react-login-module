e63222de88d6d3eac20f8ff51d78f7059343054a
Suraj KY <face.surajyadav@gmail.com>
2022-08-04 22:11:06 +0530

    add Route for AdminPage in App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router';
 
 const LoginPage = lazy(() => import('./components/LoginPage'));
+const AdminPage = lazy(() => import('./components/AdminPage'));
 const ErrorPage = lazy(() => import('./components/ErrorPage'));
 
 function App() {
@@ -10,6 +11,11 @@ function App() {
       <Route path='/' element={
         <Suspense fallback={<p>Loading...</p>}>
           <LoginPage />
+        </Suspense>
+      } />
+      <Route path='/admin' element={
+        <Suspense fallback={<p>Loading...</p>}>
+          <AdminPage />
         </Suspense>
       } />
       <Route path='*' element={
