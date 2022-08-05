d5e24e0240279659bfcbab10c8fa9c68ab2ef114
Suraj KY <face.surajyadav@gmail.com>
2022-08-05 14:56:38 +0530

    add check login system in AdminPage

--- src/components/AdminPage.js
+++ src/components/AdminPage.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router';
 
 const AdminPage = () => {
 
-    const navigate = useNavigate();    
+    const [checkLogin, setCheckLogin] = useState(false);
+    const token = localStorage.getItem('token');
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        token ? setCheckLogin(true) : navigate('/');
+    }, [token, navigate]);
 
     // logoutHandle
     const logoutHandle = () => {
         localStorage.removeItem('token');
-        navigate('/'); 
+        navigate('/');
     }
-    return (<>
-        <h1>AdminPage</h1>
-        <h2 onClick={logoutHandle}>Logout</h2>
-    </>)
+    return (<>{
+        checkLogin ? <>
+            <h1>AdminPage</h1>
+            <h2 onClick={logoutHandle}>Logout</h2>
+        </> : null
+    }</>)
 }
 
 export default AdminPage
