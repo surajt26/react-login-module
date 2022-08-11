cf71f8ffb938dfefa3c7b6197e8619cdc103182f
Suraj KY <face.surajyadav@gmail.com>
2022-08-12 00:02:30 +0530

    applied bootstrap and add show/hide password system and update checkBox

--- src/components/loginPage.js
+++ src/components/loginPage.js
@@ -1,22 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router';
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import logo from '../images/logo.png';
 
 const LoginPage = () => {
-
+    
     // useState for check is user login
     const [checkLogin, setCheckLogin] = useState(false);
+    // useState for store loginForm inputs
+    const [loginForm, setLoginForm] = useState({});
+    // useState for password show/hide
+    const [showBtn, setShowBtn] = useState(true);
+    // useState for checkbox
+    const [checked, setChecked] = useState(false)
     // get the token from localStorage
     const token = localStorage.getItem('token');
     const navigate = useNavigate();
 
-    // check login system
+    // check user do logedin
     useEffect(() => {
         !token ? setCheckLogin(true) : navigate('/admin');
     }, [token, navigate]);
-
-    // useState for store loginForm inputs
-    const [loginForm, setLoginForm] = useState({});
 
     // getInputHandle for handle input onChange
     const getInputHandle = (event) => {
@@ -32,7 +38,6 @@ const LoginPage = () => {
     const loginFormHandle = async (event) => {
         // stop default behavior
         event.preventDefault();
-        console.log(loginForm);
         // post the data by api
         await axios.post(`https://reqres.in/api/login`, loginForm)
             .then(response => {
@@ -46,36 +51,89 @@ const LoginPage = () => {
                 console.log(error);
                 alert('Invalid Email or Password, Try again');
             });
-        // setLoginForm({});
+        setLoginForm({});
+        setChecked(false);
     };
 
     return (<>
         {
             checkLogin ? <>
-                <form name='loginForm' onSubmit={loginFormHandle}>
-                    <label htmlFor='Email'>Email</label><br />
-                    <input
-                        type='email'
-                        id='Email'
-                        name='email'
-                        value={loginForm.email || ''}
-                        onChange={getInputHandle}
-                        required
-                    /><br />
-                    <label htmlFor='Password'>Password</label><br />
-                    <input
-                        type='password'
-                        id='Password'
-                        name='password'
-                        value={loginForm.password || ''}
-                        onChange={getInputHandle}
-                        required
-                    /><br />
-                    <input
-                        type='submit'
-                        value='Login'
-                    />
-                </form>
+                {/* main section */}
+                <div className="container">
+                    {/* login section */}
+                    <div className="row vh-100">
+                        <div className="col-12 col-sm-8 col-md-6 col-lg-5 col-xl-4 mx-auto my-auto">
+                            {/* logo section */}
+                            <div className="mb-2">
+                                <img src={logo} alt='logoPicture' />
+                            </div>
+                            {/* welcome text section */}
+                            <div className="wishText mb-4">
+                                <p>Hello there, Sing In to Continue</p>
+                            </div>
+                            {/* form begin */}
+                            <form name='loginForm' onSubmit={loginFormHandle}>
+                                {/* email input */}
+                                <div className='form-group'>
+                                    <label htmlFor='Email' className='text-secondary mb-0 formLabel'>Email</label><br />
+                                    <input
+                                        type='email'
+                                        id='Email'
+                                        className='form-control py-4'
+                                        name='email'
+                                        value={loginForm.email || ''}
+                                        onChange={getInputHandle}
+                                        required
+                                    />
+                                </div>
+                                {/* password input */}
+                                <div className='form-group'>
+                                    <label htmlFor='Password' className='text-secondary mb-0 formLabel' >Password</label><br />
+                                    <div className='input-group'>
+                                        <input
+                                            type={showBtn ? 'password' : 'text'}
+                                            id='Password'
+                                            className='form-control py-4 border-right-0'
+                                            name='password'
+                                            value={loginForm.password || ''}
+                                            onChange={getInputHandle}
+                                            required
+                                        />
+                                        {/* password show/hide button */}
+                                        <div className="input-group-append">
+                                            <span
+                                                className="input-group-text bg-white"
+                                                onClick={() => setShowBtn(() => showBtn ? false : true)}
+                                            >
+                                                {showBtn ? <Visibility /> : <VisibilityOff />}
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                                {/* agreement checkbox input */}
+                                <div className='form-check'>
+                                    <input
+                                        type="checkbox"
+                                        id="agreement"
+                                        className='form-check-input text-secondary'
+                                        name="agreement"
+                                        checked={checked}
+                                        onChange={() => setChecked(() => checked ? false : true)}
+                                        required
+                                    />
+                                    <label className='form-check-label text-secondary agreeText' htmlFor='agreement'>By creating or logging into an account, you are agreeing with our
+                                        <span className='font-weight-bold'> Terms and Conditions</span> and
+                                        <span className='font-weight-bold'> Privacy Policy's.</span>
+                                    </label>
+                                </div>
+                                {/* submint button */}
+                                <div className='form-group mt-4'>
+                                    <button className='btn btn-light btn-block submitBtn' type='submit'>Next</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </> : null
         }
     </>)
